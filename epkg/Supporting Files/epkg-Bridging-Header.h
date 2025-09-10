7ae46d467c893d23700cb2a951e102913adcf6b2
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2025-09-09 18:20:17 -0700

    innit

---
+++ epkg/Supporting Files/epkg-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+#include "PKBOM.h"
+
+#include "PKBundleComponent.h"
+#include "PKBundleComponentVersion.h"
+
+#include "PKComponent.h"
+
+#include "PKPackage.h"
+#include "PKPackageChecker.h"
+#include "PKPackageInfo.h"
+
+#include "PKInstallHistory.h"
+#include "PKProductInfo.h"
+#include "PKReceipt.h"
